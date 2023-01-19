56845077f20eeea37c69be10f113b81ccd2c64eb
Ashjag47 <Ashish_P@mckinsey.com>
2023-01-19 11:07:16 +0530

    Feat: Add test cases to BowlingScoreTracker using jest

--- BowlingScoreTracker.test.js
+++ BowlingScoreTracker.test.js
@@ -1,11 +1,22 @@
 const totalScore = require("./BowlingScoreTracker");
+const bestScore = require("./BowlingScoreTracker");
 
-const cases = [
+const cases = {
+    quest1:[
   [[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6], 90],
   [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10], 30],
-  [[6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 16]
-];
+  [[6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 16]],
+  quest2:[
+    [[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10],
+  [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], 90]
+};
 
-test.each(cases)("Total score of %p is %d", (input, output) => {
+test.each(cases.quest1)("Total score of %p is %d", (input, output) => {
   expect(totalScore(input)).toBe(output);
 });
+console.log(cases.quest2[0])
+
+test("Best score is", () => {
+    expect(bestScore(cases.quest2[0])).toBe(cases.quest2[1]);
+  });
