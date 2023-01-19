87c47b36359ad456c51fbee18b4bf270529da08f
Ashjag47 <Ashish_P@mckinsey.com>
2023-01-19 18:38:30 +0530

    Feat: Add test cases to BowlingScoreTracker using jest

--- BowlingScoreTracker.test.js
+++ BowlingScoreTracker.test.js
@@ -2,21 +2,24 @@ const totalScore = require("./BowlingScoreTracker");
 const bestScore = require("./BowlingScoreTracker");
 
 const cases = {
-    quest1:[
-  [[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6], 90],
-  [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10], 30],
-  [[6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 16]],
-  quest2:[
+  quest1: [
+    [[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6], 90],
+    [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10], 30],
+    [[6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 16]],
+  quest2: [
     [[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10],
-  [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], 90]
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10],
+      [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], 90]
 };
+
+
 
 test.each(cases.quest1)("Total score of %p is %d", (input, output) => {
   expect(totalScore(input)).toBe(output);
 });
-console.log(cases.quest2[0])
+//console.log(typeof(Object.entries(cases.quest2[0])));
+console.log(Array.isArray(cases.quest2[0]))
 
 test("Best score is", () => {
-    expect(bestScore(cases.quest2[0])).toBe(cases.quest2[1]);
-  });
+  expect(bestScore(cases.quest2[0])).toBe(cases.quest2[1]);
+});
