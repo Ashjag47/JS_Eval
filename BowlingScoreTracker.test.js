56db7bc010bf2450f16d83fd62a26f7de771a347
Ashjag47 <Ashish_P@mckinsey.com>
2023-01-19 10:12:08 +0530

    Feat: Add test cases to BowlingScoreTracker using jest

---
+++ BowlingScoreTracker.test.js
@@ -0,0 +1,11 @@
+const totalScore = require("./BowlingScoreTracker");
+
+const cases = [
+  [[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6], 90],
+  [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10], 30],
+  [[6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 16]
+];
+
+test.each(cases)("Total score of %p is %d", (input, output) => {
+  expect(totalScore(input)).toBe(output);
+});
